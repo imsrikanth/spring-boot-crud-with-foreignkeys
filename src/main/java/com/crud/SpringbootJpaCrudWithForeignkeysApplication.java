13d757ddfe3e44803f801a494e98d71b8a5fd5c4
Srikanth <srikanthseeram047@gmail.com>
2024-10-08 22:11:42 +0530

    initial commit

---
+++ src/main/java/com/crud/SpringbootJpaCrudWithForeignkeysApplication.java
@@ -0,0 +1,13 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCrudWithForeignkeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaCrudWithForeignkeysApplication.class, args);
+	}
+
+}
